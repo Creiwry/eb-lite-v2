b5e30baeda94a8848ab73257167686260751d6a3
Creiwry <jay.gardos@outlook.com>
2022-12-08 11:56:10 +0100

    feat(validators): add DivisibleByFive validator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
+require_relative './validators/divisible_by_five_validator'
 
 class Event < ApplicationRecord
   validates :start_time, presence: true, comparison: { greater_than: Time.now }
-  validates :duration, presence: true, numericality: { greater_than: 0 }, divisible_by_five: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
   validates :title, prasence: true, length: { in: 5..140 }
   validates :description, presence: true, length: { in: 20..1000 }
   validates :price, presence: true, numericality: { in: 1..1000 }
   validates :location, presence: true
+  validates_with DivisibleByFive
   has many :attendances
   has_many :participants, class_name: 'User', through: :attendances
   belongs_to :administrator, class_name: 'User'
---
+++ app/models/validators/divisible_by_five_validator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# IsPositiveIntegerDivisibleByFive custom validator
+class DivisibleByFive < ActiveModel::Validator
+  def validate(record)
+    return if (record.duration % 5).zero?
+
+    record.errors.add :zero, (options[:message] || 'has to be divisible by 5')
+  end
+end
